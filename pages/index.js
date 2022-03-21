dd13b0b2d89673e96be983773a9d932344feafe9
austinkelsay <austinkelsay@yahoo.com>
2022-03-21 17:15:06 -0600

    Trying another trick

--- pages/index.js
+++ pages/index.js
@@ -323,10 +323,14 @@ export default function Home() {
       `}</style>
 
       <style jsx global>{`
+        #__next,
         html,
         body {
-          min-height: 100vh;
-          min-height: -webkit-fill-available;
+          position: absolute;
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
           padding: 0;
           margin: auto;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
