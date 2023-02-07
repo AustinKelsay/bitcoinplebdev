f236efed6fd43a6726e4f920754f9358705d073b
Austin Kelsay <austinkelsay@yahoo.com>
2023-02-07 17:25:27 -0600

    Trying to add nip5 server

---
+++ nip5.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+
+const nostrData = {
+  names: {
+    bitcoinplebdev:
+      "8172b9205247ddfe99b783320782d0312fa305a199fb2be8a3e6563e20b4f0e2",
+  },
+};
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("/.well-known/nostr.json", (req, res) => {
+    res.json({
+      nostrData,
+    });
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:3000");
+  });
+});
