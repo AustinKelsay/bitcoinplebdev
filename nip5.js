d434cb1bb7fa08996250b8ee3655480fa4a47e1e
Austin Kelsay <austinkelsay@yahoo.com>
2023-02-07 18:33:35 -0600

    Updated express init

--- nip5.js
+++ nip5.js
@@ -3,25 +3,29 @@ const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const nostrData = {
-  names: {
-    bitcoinplebdev:
-      "8172b9205247ddfe99b783320782d0312fa305a199fb2be8a3e6563e20b4f0e2",
-  },
-};
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-app.prepare().then(() => {
-  const server = express();
-
-  server.get("/.well-known/nostr.json", (req, res) => {
-    res.json({
-      nostrData,
+    server.get("*", (req, res) => {
+      return handle(req, res);
     });
-  });
 
-  return server.listen(3000, (err) => {
-    if (err) throw err;
-    console.log("> my-nip5");
+    server.get("/.well-known/nostr.json", (req, res) => {
+      res.status(200).json({
+        nostrData,
+      });
+    });
+
+    server.listen(3000, (err) => {
+      if (err) throw err;
+      console.log("my-nip5");
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
   });
-});
