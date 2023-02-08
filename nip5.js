98f2b2bf236cbff7300724876f506bdd7b51a55b
Austin Kelsay <austinkelsay@yahoo.com>
2023-02-07 18:25:01 -0600

    Updating return

--- nip5.js
+++ nip5.js
@@ -20,9 +20,8 @@ app.prepare().then(() => {
     });
   });
 
-  server.listen(3000, (err) => {
+  return server.listen(3000, (err) => {
     if (err) throw err;
     console.log("> my-nip5");
-    return;
   });
 });
